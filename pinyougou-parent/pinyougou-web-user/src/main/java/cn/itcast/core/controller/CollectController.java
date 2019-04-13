18dc73f47457668c67e7fca65ac4ad454c2312a2
FWdalaoshi <1107971904@qq.com>
2019-04-13 23:09:28 +0800

    测试

---
+++ pinyougou-parent/pinyougou-web-user/src/main/java/cn/itcast/core/controller/CollectController.java
@@ -0,0 +1,24 @@
+package cn.itcast.core.controller;
+
+import cn.itcast.core.pojo.item.Item;
+import cn.itcast.core.service.CollectService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/collect")
+public class CollectController {
+
+    @Reference
+    private CollectService collectService;
+
+    @RequestMapping("/findList")
+    public List<Item> findList(){
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        return collectService.findList(name);
+    }
+}
