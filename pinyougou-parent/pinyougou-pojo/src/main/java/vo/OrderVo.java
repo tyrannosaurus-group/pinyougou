466da8d05d72379d64013054e8002409c4c216b4
Tim_Smith <SaBeiFeng@aliyun.com>
2019-04-14 14:55:55 +0800

    解决ordervo冲突

--- pinyougou-parent/pinyougou-pojo/src/main/java/vo/OrderVo.java
+++ pinyougou-parent/pinyougou-pojo/src/main/java/vo/OrderVo.java
@@ -51,7 +51,30 @@ public class OrderVo implements Serializable{
     //商品操作
     private Integer sendstatus;
     //交易状态
+//订单id
 
+    //商品名称
+
+    //商品价格
+
+    //商品数量
+
+    //订单实付金额
+
+    //订单来源
+    /**
+     * 订单来源：1:app端，2：pc端，3：M端，4：微信端，5：手机qq端
+     */
+
+    //创建时间
+    /**
+     * 订单创建时间
+     */
+
+    //付款状态
+    /**
+     * 状态：1、未付款，2、已付款，3、未发货，4、已发货，5、交易成功，6、交易关闭,7、待评价
+     */
 
     public List<String> getSpec() {
         return spec;
@@ -183,102 +206,4 @@ public class OrderVo implements Serializable{
 
 
 
-
-public class OrderVo implements Serializable {
-    //订单id
-    private long id;
-
-    //商品名称
-    private String goodsName;
-
-    //商品价格
-    private BigDecimal price;
-
-    //商品数量
-    private int num;
-
-    //订单实付金额
-    private BigDecimal totalFee;
-
-    //订单来源
-    /**
-     * 订单来源：1:app端，2：pc端，3：M端，4：微信端，5：手机qq端
-     */
-    private String sourceType;
-
-    //创建时间
-    /**
-     * 订单创建时间
-     */
-    private Date createTime;
-
-    //付款状态
-    /**
-     * 状态：1、未付款，2、已付款，3、未发货，4、已发货，5、交易成功，6、交易关闭,7、待评价
-     */
-    private String status;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getGoodsName() {
-        return goodsName;
-    }
-
-    public void setGoodsName(String goodsName) {
-        this.goodsName = goodsName;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public int getNum() {
-        return num;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
-    }
-
-    public BigDecimal getTotalFee() {
-        return totalFee;
-    }
-
-    public void setTotalFee(BigDecimal totalFee) {
-        this.totalFee = totalFee;
-    }
-
-    public String getSourceType() {
-        return sourceType;
-    }
-
-    public void setSourceType(String sourceType) {
-        this.sourceType = sourceType;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
